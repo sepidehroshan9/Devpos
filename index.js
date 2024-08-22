5971466c5d02a9fdc2aa66cbf71ddb9ca2e2d9c2
sepidehroshan <sepidehroshan9@gmail.com>
2024-08-22 15:33:26 +0330

    add to github

---
+++ index.js
@@ -0,0 +1,7 @@
+const express=require('express');
+const app=express();
+const port=8080;
+app.get('/',(req,res)=>{
+    res.send('Hello world!');});
+
+    app.listen(port,()=>{console.log('Server is running ot  http://localhost:${port?/');})
