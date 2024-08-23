cd04013b0f6ad71819cc0838820a04b51a2dd6f2
sepidehroshan <sepidehroshan9@gmail.com>
2024-08-23 12:20:37 +0330

    port

--- index.js
+++ index.js
@@ -4,5 +4,6 @@ const port=8080;
 app.get('/',(req,res)=>{
     res.send('Hello world!');});
 
-    app.listen(port,()=>{console.log('Server is running ot  http://localhost:${port}/');
+    app.listen(port,()=>{
+        console.log(`Server is running ot  http://localhost:${port}/`);
 });
